99a84dec1f9600feba215227c515e5e355f51ee4
sdosljak1 <sdosljak1@etf.unsa.ba>
2023-09-19 11:04:45 +0200

    Biljeska Dao

--- src/main/java/ba/unsa/etf/rpr/dao/DaoBiljeska.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoBiljeska.java
@@ -3,22 +3,33 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.Biljeska;
 import ba.unsa.etf.rpr.domain.Vlasnik;
 import ba.unsa.etf.rpr.domain.Kategorija;
+import java.util.List;
 
 public interface BiljeskaDao extends Dao<Biljeska> {
+    /**
+     * Returns all quotes that contains given text.
+     *
+     * @param text search string for quotes
+     * @return list of quotes
+     */
+    List<Biljeska> searchByText(String text) throws QuoteException;
+
+    /**
+     * Returns all quotes that contains given text.
+     *
+     * @param kategorija search string for quotes
+     * @return list of quotes
+     */
+    List<Biljeska> searchByCategory(Kategorija kategorija) throws QuoteException;
+
+    /**
+     * Return random quote from database
+     *
+     * @return random quote
+     * @throws QuoteException
+     */
+
+    List<Biljeska> searchByOwner(Vlasnik vlasnik) throws QuoteException;
+
+    Biljeska randomQuote() throws QuoteException;
 }
-
-/*
-public interface Biljeska {
-    void setNaslov(String naslov);
-    void setSadrzaj(String sadrzaj);
-    void setVlasnik(Vlasnik vlasnik);
-    void setKategorija(Kategorija kategorija);
-    void setDatum(Date datum);
-
-    String getNalov();
-    String getSadrzaj();
-    Vlasnik getVlasnik();
-    Kategorija getKazegorija();
-    Date getDatum();
-    }
- */

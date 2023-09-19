99c00cbd32fa8f60e01804b7d63bb50941a475e1
sdosljak1 <sdosljak1@etf.unsa.ba>
2023-09-19 12:15:02 +0200

    KategorijaDaoSQLImplementation

---
+++ src/main/java/ba/unsa/etf/rpr/dao/KategorijaDaoSQLImpl.java
@@ -0,0 +1,45 @@
+package ba.unsa.etf.rpr.dao;
+import ba.unsa.etf.rpr.domain.Kategorija;
+import ba.unsa.etf.rpr.exceptions.QuoteException;
+
+import java.sql.*;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class KategorijaDaoSQLImpl extends AbstractDao<Kategorija> implements KategorijaDao{
+    private static  KategorijaDaoSQLImpl instance = null;
+    private KategorijaDaoSQLImpl() {
+        super("kategorija");
+    }
+
+    public static KategorijaDaoSQLImpl getInstance(){
+        if(instance==null)
+            instance = new KategorijaDaoSQLImpl();
+        return instance;
+    }
+
+    public static void removeInstance(){
+        if(instance!=null)
+            instance=null;
+    }
+
+    @Override
+    public Kategorija row2object(ResultSet rs) throws QuoteException {
+        try {
+            Kategorija cat = new Kategorija();
+            cat.setId(rs.getInt("id"));
+            cat.setNaziv(rs.getString("naziv"));
+            return cat;
+        } catch (SQLException e) {
+            throw new QuoteException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public Map<String, Object> object2row(Kategorija object) {
+        Map<String, Object> row = new TreeMap<>();
+        row.put("id", object.getId());
+        row.put("naziv", object.getNaziv();
+        return row;
+    }
+}

c6d7eb59c5806c1a1f8a7924344cbdf4ffa6b5bb
sdosljak1 <sdosljak1@etf.unsa.ba>
2023-09-19 06:45:54 +0200

    Biljeske interface setteri ispravka

--- src/main/java/ba/unsa/etf/rpr/Biljeska.java
+++ src/main/java/ba/unsa/etf/rpr/Biljeska.java
@@ -1,9 +1,11 @@
 package ba.unsa.etf.rpr;
 
+import java.util.Date;
+
 public interface Biljeska {
-    void setNaslov();
-    void setSadrzaj();
-    void setVlasnik();
-    void setKategorija();
-    void setDatum();
+    void setNaslov(String naslov);
+    void setSadrzaj(String sadrzaj);
+    void setVlasnik(Vlasnik vlasnik);
+    void setKategorija(Kategorija kategorija);
+    void setDatum(Date datum);
 }
